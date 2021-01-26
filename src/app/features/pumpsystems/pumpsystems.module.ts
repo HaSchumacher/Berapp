fe24af93cf7d8c2010e8523d62e012b2fd2acba4
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-01-26 19:03:24 +0100

    generated feature module

---
+++ src/app/features/pumpsystems/pumpsystems.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PumpsystemsRoutingModule } from './pumpsystems-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PumpsystemsRoutingModule
+  ]
+})
+export class PumpsystemsModule { }
